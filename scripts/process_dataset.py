bfe767855754fa3b4566037d8b0ca9ff02d91321
yunkai1841 <70383706+yunkai1841@users.noreply.github.com>
2023-07-02 13:38:28 +0900

    update script for format dataset

--- scripts/process_dataset.py
+++ scripts/process_dataset.py
@@ -1,5 +1,7 @@
 # %%
 from datasets import load_dataset
+import tqdm
+import json
 
 # the dataset needs to be manually downloaded from
 # https://recipenlg.cs.put.poznan.pl/
@@ -10,6 +12,29 @@ dataset = load_dataset("recipe_nlg", data_dir=dataset_dir)
 # %%
 # print the dataset info
 print(dataset)
+
 # %%
-# print the first 5 examples
-print(dataset["train"][:5])
+# format the dataset into alpaca format
+
+instruction_format = "How to make '{}' from the following ingredients?"
+json_data = []
+for data in dataset["train"]:
+    instruction = instruction_format.format(data["title"])
+    output = ""
+    for i, direction in tqdm(enumerate(data["directions"])):
+        output += f"{i+1}. {direction}\n"
+    input = str(data["ner"])
+    input = input[1:-1] # remove the square brackets
+
+    json_data.append(
+        {
+            "instruction": instruction,
+            "input": input,
+            "output": output,
+        }
+    )
+
+# %%
+# save the dataset
+with open("recipe_nlg.json", "w") as f:
+    json.dump(json_data, f, indent=4)
