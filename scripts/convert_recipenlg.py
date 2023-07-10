8cfe6c449e4edcb53e99d2495de55eec656d4312
zhuyunkai <zhuyunkai@kasahara.cs.waseda.ac.jp>
2023-07-11 02:05:06 +0900

    change method to convert dataset

--- scripts/convert_recipenlg.py
+++ scripts/convert_recipenlg.py
@@ -6,7 +6,7 @@ import json
 # the dataset needs to be manually downloaded from
 # https://recipenlg.cs.put.poznan.pl/
 # replace the path below with the path to the dataset
-dataset_dir = ""
+dataset_dir = "data"
 dataset = load_dataset("recipe_nlg", data_dir=dataset_dir)
 
 # %%
@@ -16,12 +16,13 @@ print(dataset)
 # %%
 # format the dataset into alpaca format
 
-instruction_format = "How to make '{}' from the following ingredients?"
+instruction_format = "Generate a recipe with these ingredients."
+
 json_data = []
 tqdm_bar = tqdm.tqdm(total=len(dataset["train"]))
 for data in dataset["train"]:
-    instruction = instruction_format.format(data["title"])
-    output = ""
+    instruction = instruction_format
+    output = data["title"]
     for i, direction in enumerate(data["directions"]):
         output += f"{i+1}. {direction}\n"
     input = str(data["ner"])
@@ -41,5 +42,5 @@ tqdm_bar.close()
 # %%
 # save the dataset
 print("Saving the dataset...")
-with open("recipe_nlg.json", "w") as f:
+with open("data/recipe_nlg.json", "w") as f:
     json.dump(json_data, f, indent=4)
